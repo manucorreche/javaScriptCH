41b8b579df171167926a5b75735ab9e733d7683d
manucorreche <manuelcorreche@gmail.com>
2022-01-12 16:02:12 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+/*
+let nombre = "Manuel";// string
+let edad = 37; // integer
+let altura = 1.84; // float o double
+*/
+/*
+let num1 = 20;
+let num2 = 21;
+let suma = num1 + num2; 
+
+console.log(suma);
+
+let nombre = "Manuel ";
+let apellido ="Correche";
+let datosCompletos = nombre+apellido;
+
+console.log(datosCompletos);
+
+
+alert("Hola a todos"); // mensaje
+//prompt("Ingresa tu nombre");//no guarda nada
+let nombreUsuario = prompt ("Ingrese su nombre de usuario"); // guarda el valor que usuario ingresa en el prompt
+alert("Hola! bienvenido " + nombreUsuario);
+*/
+/*
+let numero = parseInt(prompt ("Ingrese un numero"))
+console.log (numero);
+if (numero <1000){alert("El numero es menor que 1000")}
+else{alert("el numero es mayor que 1000")}
+*/
+/*let saludo = prompt("Diga Hola");
+if (saludo.toUpperCase () === "HOLA") {alert("escribiste Hola")}*/
+
+/*
+let num1 = 12 //parseInt(prompt("Digite un numero"))
+let num2 = 14 //parseInt(prompt("Digite un numero"))
+let num3 = 18 //parseInt(prompt("Digite un numero"))
+
+if(num1>num2 && num2>num3){console.log(num1 + " es el mayor")}
+else if (num2>num1 && num2>num3) {console.log(num2 + "es el mayor")}
+else{console.log(num3 + " es el mayor")}  
+*/
+
+let dato = 2;
+
+    for (let i = 0; i >=10; i++) 
+    {
+    let resultado = dato * i
+    console.log(dato + " x "+ i + " = " + resultado)
+}
